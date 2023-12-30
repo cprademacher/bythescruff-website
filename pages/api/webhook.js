985f72d29953e3da042ceef618ef1a926e032ab9
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-12-30 17:51:58 -0600

    Started on products page

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,9 +1,10 @@
 import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/Order";
 const stripe = require("stripe")(process.env.STRIPE_SK);
 import { buffer } from "micro";
 
 const endpointSecret =
-  "whsec_338b2b943931188e1789b73f7523616ea0d78c14c72a4dc865599250108d4e91";
+  "whsec_376b1bdcab3478b7aa2152ad1ef13b26726836fd6bacabce722dab678147c492";
 
 export default async function handler(req, res) {
   await mongooseConnect();
@@ -26,12 +27,20 @@ export default async function handler(req, res) {
   // Handle the event
   switch (event.type) {
     case "chekout.session.succeeded":
-      const paymentIntentSucceeded = event.data.object;
-      console.log(paymentIntentSucceeded);
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, {
+          paid: true,
+        });
+      }
       break;
     default:
       console.log(`Unhandled event type ${event.type}`);
   }
+
+  res.status(200).send("OK");
 }
 
 export const config = {
---
+++ pages/products.js
@@ -0,0 +1,18 @@
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+`;
+
+export default function ProductsPage() {
+  return (
+    <>
+      <Header />
+      <Center>
+        <Title>All Products</Title>
+      </Center>
+    </>
+  );
+}
